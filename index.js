b3eef11c026408f11d54be3dae3faeb711b484ff
chuck1z <71697225+chuck1z@users.noreply.github.com>
2023-04-12 21:19:43 +0000

    help me please

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const tf = require('@tensorflow/tfjs-node');
+const os = require('os');
+const path = require('path');
+require('dotenv').config()
+
+const app = express(); // create a new Express app instance
+
+class Item {
+  constructor(sl, sw, pl, pw) {
+    this.sl = sl;
+    this.sw = sw;
+    this.pl = pl;
+    this.pw = pw;
+  }
+}
+
+const port = parseInt(process.env.PORT);
+
+const loadModel = async () => {
+  const model = await tf.loadLayersModel('file://' + path.join(__dirname, 'model/model.json'));
+  return model;
+}
+
+let model;
+
+loadModel().then(m => model = m);
+
+function predict(sl, sw, pl, pw) {
+  let resmessage = 'undefined';
+  const temp = tf.tensor2d([[sl, sw, pl, pw]]);
+  const result = model.predict(temp);
+  const clas = result.argMax(1).dataSync()[0];
+
+  if (clas === 0) {
+    resmessage = 'Iris-setosa';
+  } else if (clas === 1) {
+    resmessage = 'Iris-versicolor';
+  } else if (clas === 2) {
+    resmessage = 'Iris-virginica';
+  } else {
+    resmessage = 'undefined';
+  }
+
+  return resmessage;
+}
+
+// middleware to parse JSON requests
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.post('/', (req, res) => {
+  const item = new Item(req.body.sl, req.body.sw, req.body.pl, req.body.pw);
+  const result = predict(item.sl, item.sw, item.pl, item.pw);
+  res.json({ result });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
